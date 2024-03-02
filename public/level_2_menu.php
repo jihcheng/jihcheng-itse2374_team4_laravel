7bd82eeb6304bbb1d7c5daf9fb85c4cda2301f7d
jihcheng <jchao4@collin.edu>
2024-03-02 01:15:42 -0600

    modified  dockerfile 5

--- public/level_2_menu.php
+++ public/level_2_menu.php
@@ -23,22 +23,31 @@ switch($level_1_menu){
     <a href="#" title="A password must be between 8 and 20 characters long and must contain at least one uppercase letter, one lowercase letter, and one number"><i style="font-size:60%" class="twa twa-2x twa-question-mark"></i></a></label>
     <input id="user_password" size="20" type="password" maxlength="20" name="user_password"/><br>
 
+    <label style="color:blue;relative;  top: 6px;">Enter decryption vector
+    <a href="#" title="for security reasons when this website go to the internet"><i style="font-size:60%" class="twa twa-2x twa-question-mark"></i></a></label>
+    <input id="decryption_iv" size="20" type="password" maxlength="20" name="decryption_iv"/><br>
+
+    <label style="color:blue;relative;  top: 6px;">Enter decryption key
+    <a href="#" title="for security reasons when this website go to the internet"><i style="font-size:60%" class="twa twa-2x twa-question-mark"></i></a></label>
+    <input id="decryption_key" size="20" type="password" maxlength="20" name="decryption_key"/><br>
+
+
     <input style="width: 80px; position: relative;  top: 6px;left:20px;" type="reset" name="reset" id="myreset" value="Reset" />
     &nbsp;&nbsp;			
     <input style="width: 80px; position: relative;  top: 6px;left:20px;" type="submit" name="submit" id="mysubmit" value="Submit" /> 
     </fieldset>
 <?php
     break;
-    case 'User_Login':
+    case 'Manage_Individual_Message':
+    
 ?>
     <fieldset style="position: relative;  top: -8px ;left:4px; font-size:85%;"><legend><?php echo str_replace("_"," ",$level_1_menu);?></legend>
     <label style="color:blue;relative;  top: 6px;">Enter Your email
-    <a href="#" title="Max 30 characters for an email address"><i style="font-size:60%" class="twa twa-2x twa-question-mark"></i></a>
+    <a href="#" title="Enter email address you registed for this website"><i style="font-size:60%" class="twa twa-2x twa-question-mark"></i></a>
     </label><br>
     <input id="user_email" size="30" type="email" maxlength="30" name="user_email"/><br>
 
-    <label style="color:blue;relative;  top: 6px;">Enter Your Password
-    <a href="#" title="A password must be between 8 and 20 characters long and must contain at least one uppercase letter, one lowercase letter, and one number"><i style="font-size:60%" class="twa twa-2x twa-question-mark"></i></a></label>
+    <label style="color:blue;relative;  top: 6px;">Enter Your Password</label>
     <input id="user_password" size="20" type="password" maxlength="20" name="user_password"/><br>
 
     <input style="width: 80px; position: relative;  top: 6px;left:20px;" type="reset" name="reset" id="myreset" value="Reset" />
@@ -47,6 +56,26 @@ switch($level_1_menu){
     </fieldset>
 <?php
 
+    break;
+    case 'Manage_Group_Message':
+?>
+        <fieldset style="position: relative;  top: -8px ;left:4px; font-size:85%;"><legend><?php echo str_replace("_"," ",$level_1_menu);?></legend>
+        <label style="color:blue;relative;  top: 6px;">Enter Your email
+        <a href="#" title="Enter email address you registed for this website"><i style="font-size:60%" class="twa twa-2x twa-question-mark"></i></a>
+        </label><br>
+        <input id="user_email" size="30" type="email" maxlength="30" name="user_email"/><br>
+    
+        <label style="color:blue;relative;  top: 6px;">Enter Your Password</label>
+        <input id="user_password" size="20" type="password" maxlength="20" name="user_password"/><br>
+
+        <button id="find_group_dropdown" type="button" onclick="group_message_next(this.value)" style="position: relative;  top: 5px ; background:#00FEC4; color:black">Next Step</button>
+        
+        </fieldset>
+        <!-- <div id="group-list"></div> -->
+        
+        
+
+<?php
     break;
 
     default:
